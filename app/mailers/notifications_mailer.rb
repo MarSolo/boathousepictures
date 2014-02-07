dcb98c89948450fe8741885d6a06fff8bf948240
Mario Maiale <mariosolo@gmail.com>
2014-02-06 21:19:20 -0500

    contact

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,6 +1,6 @@
 class NotificationsMailer < ActionMailer::Base
 
-  default :from => "noreply@youdomain.dev"
+  default :from => "info@bhpictures.com"
   default :to => "you@youremail.dev"
 
   def new_message(message)
